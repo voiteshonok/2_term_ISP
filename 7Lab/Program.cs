c491b6aa380036025d431e125e1c3fbfcb655d22
Slav <vsvoiteshonok1@gmail.com>
2020-05-16 01:22:01 +0300

    7th without parse

---
+++ 7Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7Lab
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr = new Fraction(5, 6);
+            Fraction tr = new Fraction(2, 3);
+            Fraction q = new Fraction(-3);
+            q++;
+            Console.WriteLine((q).ToString());
+            Console.WriteLine();
+
+        }
+    }
+}
